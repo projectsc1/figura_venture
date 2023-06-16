1e3ea47d1a3677cb843faa3f5e7d8a41f9d490bf
Project <87567994+projectsc1@users.noreply.github.com>
2023-06-16 15:08:58 +0100

    fixed some stuff

--- dimple/script.lua
+++ dimple/script.lua
@@ -2,8 +2,12 @@ local ArmorAPI=require("scripts.armor.KattArmorAPI")
 
 ArmorAPI.addHelmet(models.model.Head.Helmet) -- select helmet parts
 
-ArmorAPI.onChange:register(function(b)
-	log(b)
+ArmorAPI.onChange:register(function(b,c,d)
+	if c == 'HELMET' and b.visible == true then
+		models.model.Head.spiritflame:setVisible(false)
+	elseif c == 'HELMET' and b.visible == false then
+		models.model.Head.spiritflame:setVisible(true)
+	end
 end)
 vanilla_model.ALL:setVisible(false) -- disable normal player model
 vanilla_model.HELD_ITEMS:setVisible(true) -- enable held items
